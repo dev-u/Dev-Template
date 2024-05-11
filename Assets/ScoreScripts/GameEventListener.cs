9ee6192e9b8b3f437aa9f96f327e107105ceb4b1
Gustavo Rocha <gprieto@neurovirtual.com>
2024-05-11 00:41:10 -0300

    Create GameEvents base
    Base scripts to handle GameEvents that update the Score board, or any other components that need communication

---
+++ Assets/ScoreScripts/GameEventListener.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CustomGameEvent : UnityEvent<Component, object>
+{
+}
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent _gameEvent;
+    public UnityEvent _response;
+
+    private void OnEnable()
+    {
+        _gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        _gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised(Component sender, object data)
+    {
+        _response.Invoke(sender, data);
+    }
+}
