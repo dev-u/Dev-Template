7581f10dccc44f3aa94a87f0e5f59ba055165a16
João Lucas <joaolucas.moraes.oliveira@gmail.com>
2024-05-18 15:10:48 -0300

    [FEAT]  - Parallax
    Código para o parallax, suporta varias camadas com velocidades individuais

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,42 +4,42 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-       
-    [SerializeField] float parallaxEffect;
+    [Header ("Os objetos devem ser maiores que a camera para melhor uso do parallax \n para criar mais de um objeto colocar pelo menos um background como entidade pai")]
+    [Header ("GameObjects dos backgrounds")] 
     [SerializeField] private GameObject[] layers;
-
+    [Header ("ParallaxEffect, segue a ordem das layers \n 1 = mesma velocidade da camera \n quanto menor o valor mais devagar a velocidade do objeto \n quanto mais maior o valor mais rapido fica o objeto")]
+    [SerializeField] private float[] parallaxEffect;
     private Camera cam;
-    private float[] lengths;
-    private float[] startPos;
-
+    private float [] length, startpos;       
+    private float temp, dist;
     void Start()
     {
-        cam = Camera.main;
-        lengths = new float[layers.Length];
-        startPos = new float[layers.Length];
-
-        for (int i = 0; i < layers.Length; i++)
+        if (layers.Length != parallaxEffect.Length)
         {
-            startPos[i] = layers[i].gameObject.transform.position.x;
-            lengths[i] = layers[i].gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+            Debug.Log ("Não há velocidade para todas as layers, ou não há layers para todas as velocidades");
+        }
+        length = new float[layers.Length];
+        startpos = new float[layers.Length];
+        
+        cam = Camera.main;
+        for (int i = 0; i < layers.Length; i++){
+            startpos[i] = layers[i].transform.position.x;
+            length[i] = layers[i].GetComponent<SpriteRenderer>().bounds.size.x;
         }
     }
 
-        
-    // Usar o lateUpdate() 
-    void Update()
+
+    void LateUpdate()
     {
-        for (int i = 0; i < layers.Length; i++)
-        {
-            float temp = cam.transform.position.x * (1 - parallaxEffect);
-            float dist = cam.transform.position.x * parallaxEffect;
-
-            layers[i].gameObject.transform.position = new Vector3(startPos[i] + dist, 
-                                                                  layers[i].gameObject.transform.position.y,
-                                                                  layers[i].gameObject.transform.position.z);
-
-            if (temp > startPos[i] + lengths[i]) startPos[i] += lengths[i];
-            else if (temp < startPos[i] - lengths[i]) startPos[i] -= lengths[i];
+        for (int i = 0; i < layers.Length; i++){
+            
+            temp = cam.transform.position.x * (1f - parallaxEffect[i]);
+            dist = cam.transform.position.x * parallaxEffect[i];
+            
+            layers[i].transform.position = new Vector3(startpos[i] + dist, layers[i].transform.position.y, layers[i].transform.position.z); 
+        
+            if (temp > startpos[i] + length[i]) startpos[i] += length[i];
+            else if (temp < startpos[i] - length[i]) startpos[i] -= length[i];
         }
     }
 }
